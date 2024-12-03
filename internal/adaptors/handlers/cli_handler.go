50301e9230712b93d277a5231a24e07ee8b6cb6b
Thirawoot.P <thirawoot.put@gmail.com>
2024-12-03 14:24:29 +0700

    not use for loop for none interaction cli

--- internal/adaptors/handlers/cli_handler.go
+++ internal/adaptors/handlers/cli_handler.go
@@ -1,11 +1,9 @@
 package handlers
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 	"task_manager/internal/ports/input"
 )
 
@@ -20,101 +18,99 @@ func NewCliHandler(uc input.TaskInputPort) *CliHandler {
 }
 
 func (h *CliHandler) CliTaskManager() {
-	scanner := bufio.NewScanner(os.Stdin)
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
+	doneCmd := flag.NewFlagSet("done", flag.ExitOnError)
+	removeCmd := flag.NewFlagSet("rm", flag.ExitOnError)
 
-	fmt.Println("\nWelcome to Task Manager")
-	fmt.Println("Command: add <task name>, list, done <task id>, rm <task id>, exit")
+	addTaskName := addCmd.String("name", "", "Name of the task to add")
+	doneId := doneCmd.Int("id", 0, "Name of the task to add")
+	removeId := removeCmd.Int("id", 0, "Name of the task to add")
 
-	for {
-		fmt.Print("> ")
-		if !scanner.Scan() {
-			break
-		}
+	if len(os.Args) < 2 {
+		fmt.Println("Expected 'add', 'list', or 'done' commands")
+		return
+	}
 
-		input := scanner.Text()
-		command := strings.Fields(input)
+	switch os.Args[1] {
 
-		if len(command) == 0 {
-			fmt.Println("Please enter a command")
-		}
-
-		switch command[0] {
-
-		case "add":
-			if len(command) < 2 {
-				fmt.Println("Usage: add <task name>")
-				continue
-			}
-			fmt.Println("test")
-
-			taskName := strings.Join(command[1:], " ")
-
-			err := h.usecase.AddTasks(taskName)
-			if err != nil {
-				fmt.Printf("Failed to add task: %s\n", err)
-				continue
-			}
-
-			fmt.Println("Add task success!")
-
-		case "list":
-			tasks, err := h.usecase.ReadTasks()
-			if err != nil {
-				fmt.Printf("Error to get tasks: %v\n", err)
-				continue
-			} else {
-				for _, task := range tasks {
-					fmt.Printf("ID: %d, Name: %s, Done: %t \n", task.ID, task.Name, task.Done)
-				}
-			}
-
-		case "done":
-			if len(command) != 2 {
-				fmt.Println("Usage: done <task id>")
-				continue
-			}
-
-			id, err := strconv.Atoi(command[1])
-			if err != nil {
-				fmt.Printf("Failed to convert data; please make sure id is integer: %s\n", err)
-				continue
-			}
-
-			err = h.usecase.TriggerTask(id)
-			if err != nil {
-				fmt.Printf("Failed to update task id %d: %s", id, err)
-				continue
-			}
-
-			fmt.Println("Update task success!")
-
-		case "rm":
-			if len(command) != 2 {
-				fmt.Println("Usage: rm <task id>")
-				continue
-			}
-
-			id, err := strconv.Atoi(command[1])
-			if err != nil {
-				fmt.Printf("Failed to convert data; please make sure id is integer: %s\n", err)
-				continue
-			}
-
-			err = h.usecase.RemoveTask(id)
-			if err != nil {
-				fmt.Printf("Failed to update task id %d: %s", id, err)
-				continue
-			}
-
-			fmt.Println("Update task success!")
-
-		case "exit":
-			fmt.Println(`Goodbye`)
+	case "add":
+		if err := addCmd.Parse(os.Args[2:]); err != nil {
+			fmt.Println("Failed to parse add cmd: %w", err)
 			return
-
-		default:
-			fmt.Println(`Invalid option, Please try again`)
-			continue
 		}
+
+		if *addTaskName == "" {
+			fmt.Println("Task name is required. Use -name <task>")
+			return
+		}
+
+		err := h.usecase.AddTasks(*addTaskName)
+		if err != nil {
+			fmt.Printf("Failed to add task: %s\n", err)
+			return
+		}
+
+		fmt.Println("Add task success!")
+
+	case "list":
+		if err := listCmd.Parse(os.Args[2:]); err != nil {
+			fmt.Println("Failed to parse list cmd: %w", err)
+			return
+		}
+
+		tasks, err := h.usecase.ReadTasks()
+		if err != nil {
+			fmt.Printf("Error to get tasks: %v\n", err)
+			return
+		} else {
+			for _, task := range tasks {
+				fmt.Printf("ID: %d, Name: %s, Done: %t \n", task.ID, task.Name, task.Done)
+			}
+		}
+
+	case "done":
+		if err := doneCmd.Parse(os.Args[2:]); err != nil {
+			fmt.Println("Failed to parse done cmd: %w", err)
+			return
+		}
+		if *doneId == 0 {
+			fmt.Println("Usage: done -id <task id>")
+			return
+		}
+
+		err := h.usecase.TriggerTask(*doneId)
+		if err != nil {
+			fmt.Printf("Failed to update task id %d: %s", *doneId, err)
+			return
+		}
+
+		fmt.Println("Update task success!")
+
+	case "rm":
+		if err := removeCmd.Parse(os.Args[2:]); err != nil {
+			fmt.Println("Failed to parse rm cmd: %w", err)
+			return
+		}
+		if *removeId == 0 {
+			fmt.Println("Usage: rm -id <task id>")
+			return
+		}
+
+		err := h.usecase.RemoveTask(*removeId)
+		if err != nil {
+			fmt.Printf("Failed to update task id %d: %s", *removeId, err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Update task success!")
+
+	case "exit":
+		fmt.Println(`Goodbye`)
+		return
+
+	default:
+		fmt.Println(`Invalid option, Please try again`)
+		os.Exit(1)
 	}
 }
